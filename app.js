904ae945d0d1efa6e38a5d4bec0936523d8536e7
Subhranshu Sekhar Choudhury <subhransuchoudhury00@gmail.com>
2022-06-15 21:11:58 +0530

    LEVEL III md5 encryption

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
+const md5 = require("md5");
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
 const app = express();
+
+console.log(md5("chintu"))
 
 app.use(express.static("public"));
 app.set("view engine","ejs");
@@ -18,7 +21,7 @@ const userSkeliton = new mongoose.Schema({
 }); // schema
 
 
-userSkeliton.plugin(encrypt,{secret:process.env.SECRET_KEY, encryptedFields:["password"]});
+// userSkeliton.plugin(encrypt,{secret:process.env.SECRET_KEY, encryptedFields:["password"]});
 
 const User = new mongoose.model("user",userSkeliton);
 
@@ -37,7 +40,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -52,7 +55,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},(err,userFound)=>{
         if(err){
